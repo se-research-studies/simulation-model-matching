91412d14d48c56071551d984637ad7a9bcfe21b6
Sebastian Melinat <s.melinat@gmail.com>
2016-04-14 17:33:31 +0200

    basic main file for command line usage

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,36 @@
+#include <unistd.h>
 
-int main(int argc, const char* argv[])
+#include <iostream>
+
+#include <NoiseSimulation/SQLiteDataSource>
+
+int main(int argc, char* argv[])
 {
+    if (argc != 5) {
+        std::cerr << "Usage: " << argv[0] << " -v VIDEOFILE -s SIMULATIONFILE" << std::endl;
+    }
 
+    std::string videoFilename;
+    std::string simulationFilename;
+
+    int opt;
+    while ((opt = getopt(argc, argv, "v:s:")) != -1) {
+        switch (opt) {
+        case 'v':
+            videoFilename = optarg;
+            break;
+        case 's':
+            simulationFilename = optarg;
+            break;
+        default:
+            exit(EXIT_FAILURE);
+            break;
+        }
+    }
+
+    std::clog << "Using video " << videoFilename << " and sim " << simulationFilename << std::endl;
+
+    NoiseSimulation::SQLiteDataSource::createInstance();
+
+    exit(EXIT_SUCCESS);
 }
