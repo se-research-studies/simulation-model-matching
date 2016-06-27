f733ba6dc15a352b50f8a840189c7c8aabee07c9
Sebastian Melinat <s.melinat@gmail.com>
2016-06-27 14:15:17 +0200

    Utils: comparison method for doubles and helper to get file name from file path

--- src/common/utils.cpp
+++ src/common/utils.cpp
@@ -2,20 +2,29 @@
 
 namespace Common {
 
-std::string Utils::removeBraces(std::string from) {
-    from.erase(0, 1);
-    from.pop_back();
-    return from;
-}
-
-uint32_t Utils::stoui(const std::string& string) {
-    uint64_t longResult = std::stoul(string);
-    uint32_t result = longResult;
-    if (result != longResult) {
-        throw std::out_of_range(string + " cannot be converted to uint32_t");
+    std::string Utils::removeBraces(std::string from) {
+        from.erase(0, 1);
+        from.pop_back();
+        return from;
     }
-    return result;
-}
 
+    uint32_t Utils::stoui(const std::string& string) {
+        uint64_t longResult = std::stoul(string);
+        uint32_t result = longResult;
+        if (result != longResult) {
+            throw std::out_of_range(string + " cannot be converted to uint32_t");
+        }
+        return result;
+    }
+
+    std::string Utils::fileName(const std::string& filePath)
+    {
+        size_t lastSlash = filePath.find_last_of("/");
+        if (lastSlash == std::string::npos) {
+            return filePath;
+        } else {
+            return filePath.substr(lastSlash + 1, std::string::npos);
+        }
+    }
 
 } // namespace Common
--- src/common/utils.h
+++ src/common/utils.h
@@ -1,24 +1,42 @@
 #pragma once
 
-#include <cstdint>
+#include <cmath>
 #include <stdexcept>
 #include <string>
 
 namespace Common {
 
-namespace Utils {
-extern std::string removeBraces(std::string from);
-extern uint32_t stoui(const std::string& string);
+    namespace Utils {
+        extern std::string removeBraces(std::string from);
+        extern uint32_t stoui(const std::string& string);
+        extern std::string fileName(const std::string& filePath);
 
-template<typename TargetInt, typename SourceInt>
-TargetInt to(SourceInt from) {
-    TargetInt result = static_cast<TargetInt>(from);
-//      if (result != from) {
-//        throw std::out_of_range("Type conversion error. From = " + std::to_string(from) + ", result = " + std::to_string(result));
-//      }
-    return result;
-}
-}
+        template <typename TargetInt, typename SourceInt>
+        TargetInt to(SourceInt from) {
+            TargetInt result = static_cast<TargetInt>(from);
+            if (result != from) {
+                throw std::out_of_range("Type conversion error. From = " + std::to_string(from) + ", result = " + std::to_string(result));
+            }
+            return result;
+        }
+
+        // Returns 1 if a > b, 0 if a == b, -1 if a < b, limited to relevantDecimalPlaces
+        template <typename T>
+        int compare(T a, T b, int relevantDecimalPlaces = 4)
+        {
+            int scaleFactor = std::pow(10, relevantDecimalPlaces);
+            int scaledA = a * scaleFactor;
+            int scaledB = b * scaleFactor;
+            if (scaledA > scaledB) {
+                return 1;
+            } else if (scaledA < scaledB) {
+                return -1;
+            } else {
+                return 0;
+            }
+        }
+
+    }
 
 } // namespace Common
 
