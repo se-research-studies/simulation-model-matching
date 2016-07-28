10ee3a811ae7c2c794a38a278fa7e30b10d5ddc7
Sebastian Melinat <s.melinat@gmail.com>
2016-07-28 16:06:45 +0200

    added Date to grouping in averages script

--- resources/averages.sql
+++ resources/averages.sql
@@ -1,4 +1,4 @@
 SELECT simulationName, correlationFile, permutationIndex, COUNT(*) AS numDataSets, AVG(averageComputationTime), AVG(minComputationTime), AVG(maxComputationTime), AVG(totalComputationTime),
 AVG(averageMemory), AVG(minMemory), AVG(maxMemory) , (AVG(maxMemory) - AVG(minMemory)) AS memoryDiff,
 AVG(leftSteerings), AVG(rightSteerings), (AVG(leftSteerings)  + AVG(rightSteerings)) AS totalSteerings,
-AVG(averageLeftSteeringAngle), AVG(averageRightSteeringAngle), AVG(ABS(maxSteeringAngle)), MAX(maxSteeringAngle) FROM simulationdata GROUP BY simulationName, correlationFile, permutationIndex
+AVG(averageLeftSteeringAngle), AVG(averageRightSteeringAngle), AVG(ABS(maxSteeringAngle)), MAX(maxSteeringAngle) FROM simulationdata GROUP BY simulationName, correlationFile, permutationIndex, DATE(date)
